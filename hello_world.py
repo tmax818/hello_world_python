e2866550d499bc4d0e848abe7eedad6240b2da4c
tmax818 <tylermaxwell661@gmail.com>
2022-10-24 07:39:51 -0700

    fin sol

--- hello_world.py
+++ hello_world.py
@@ -1,17 +1,21 @@
 your_code_here = None
 
-# 1. TASK: print "Hello World
-print(your_code_here)
-# 2. print "Hello Noelle!" with the name in a variable
+# 1. TODO: print "Hello World"
+print("Hello World")
+# 2. TODO print "Hello Noelle!" with the name in a variable
 name = "Noelle"
-print(your_code_here)  # with a comma
-print(your_code_here)  # with a +
-# 3. print "Hello 42!" with the number in a variable
+print("Hello", name)  # with a comma
+print("Hello " + name)  # with a +
+# 3. TODO print "Hello 42!" with the number in a variable
 name = 42
-print(your_code_here)  # with a comma
-print(your_code_here)  # with a +	-- this one should give us an error!
-# 4. print "I love to eat sushi and pizza." with the foods in variables
+print("Hello", name)  # with a comma
+print("Hello " + str(name))  # with a +	-- this one should give us an error!
+# 4. TODO print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print(your_code_here)  # with .format()
-print(your_code_here)  # with an f string
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))  # with .format()
+print(f"I love to eat {fave_food1} and {fave_food2}.")  # with an f string
+
+## TODO Ninja Bonus
+
+print("I love to eat %s and %s." % (fave_food1, fave_food2))
