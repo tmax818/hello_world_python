2ee0167f36c42f7ff5e8856d715993ce2c62dfb8
tmax818 <tylermaxwell661@gmail.com>
2022-10-24 07:42:10 -0700

    update with todos

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,16 @@
 your_code_here = None
 
-# 1. TASK: print "Hello World
+# 1. TODO : print "Hello World
 print(your_code_here)
-# 2. print "Hello Noelle!" with the name in a variable
+# 2. TODO print "Hello Noelle!" with the name in a variable
 name = "Noelle"
 print(your_code_here)  # with a comma
 print(your_code_here)  # with a +
-# 3. print "Hello 42!" with the number in a variable
+# 3. TODO print "Hello 42!" with the number in a variable
 name = 42
 print(your_code_here)  # with a comma
 print(your_code_here)  # with a +	-- this one should give us an error!
-# 4. print "I love to eat sushi and pizza." with the foods in variables
+# 4. TODO print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 print(your_code_here)  # with .format()
